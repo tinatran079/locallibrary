1d36eacda9afdc6c7e8d92e8f18dd81ea2b12c59
Tina Tran <tinatran079@gmail.com>
2024-01-26 15:00:22 -0600

    author model

---
+++ express-locallibrary-tutorial/models/author.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
+
+// Virtual for author's full name
+AuthorSchema.virtual("name").get(function () {
+  // avoid errors in cases where author does not have family or first name
+  // return an empty string for this case
+  let fullname = "";
+  if (this.first_name && this.family_name) {
+    fullname = `${this.family_name}, ${this.first_name}`;
+  }
+  return fullname;
+});
+
+// Virtual for author's URL
+AuthorSchema.virtual("url").get(function () {
+  return `/catalog/author/${this._id}`;
+});
+
+//Export model
+module.exports = mongoose.model("Author", AuthorSchema);
