e00747d5a7db2a0eaa28fd9a3585bd34054515b4
Tina Tran <tinatran079@gmail.com>
2024-01-31 14:41:31 -0600

    updated index route

--- express-locallibrary-tutorial/routes/index.js
+++ express-locallibrary-tutorial/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res) {
+  // redirect to new index page in path '/catalog'
+  res.redirect("/catalog");
 });
 
 module.exports = router;
