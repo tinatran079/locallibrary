3d0256b1d582f81459e65898ae65e3f4adc131b1
Tina Tran <tinatran079@gmail.com>
2024-02-01 13:53:00 -0600

    author controller

--- express-locallibrary-tutorial/controllers/authorController.js
+++ express-locallibrary-tutorial/controllers/authorController.js
@@ -1,42 +1,42 @@
 const Author = require("../models/author");
 const asyncHandler = require("express-async-handler");
 
-// Display list of all authors
+// Display list of all Authors.
 exports.author_list = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Author list");
 });
 
-// Display detail page for specific author
+// Display detail page for a specific Author.
 exports.author_detail = asyncHandler(async (req, res, next) => {
   res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
 });
 
-// Display Author create form on GET
+// Display Author create form on GET.
 exports.author_create_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Author create GET");
 });
 
-// Handle Author create on POST
+// Handle Author create on POST.
 exports.author_create_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Author create POST");
 });
 
-// Display Author delete form on GET
+// Display Author delete form on GET.
 exports.author_delete_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Author delete GET");
 });
 
-// Handle Author delete on POST
+// Handle Author delete on POST.
 exports.author_delete_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Author delete POST");
 });
 
-// Display Author update form on GET
-exports.author_update_get = async(async (req, res, next) => {
+// Display Author update form on GET.
+exports.author_update_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Author update GET");
 });
 
-// Handle Author update on POST
+// Handle Author update on POST.
 exports.author_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLMENTED: Author update POST");
+  res.send("NOT IMPLEMENTED: Author update POST");
 });
